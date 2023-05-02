e46d57e8fa869bfcdce16c8f3633248851508c8b
Ramon Victor Cardoso <ramonvc19@gmail.com>
2023-05-01 21:07:58 -0300

    Form component from functional to class-based

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,17 +1,20 @@
+import React from "react";
 import "./Form.css";
 import TextField from "../TextField";
 
-const form = () => {
-	return (
-		<section className="form">
-			<form>
-                <h2>Preenche os dados para criar o card do jogador</h2>
-				<TextField label="Nome" placeholder="Digite seu nome" />
-				<TextField label="Cargo" placeholder="Digite seu cargo" />
-				<TextField label="Imagem" placeholder="Digite o endereço da imagem" />
-			</form>
-		</section>
-	);
+class Form extends React.Component  {
+    render() {
+        return (
+            <section className="form">
+                <form>
+                    <h2>Preenche os dados para criar o card do jogador</h2>
+                    <TextField label="Nome" placeholder="Digite seu nome" />
+                    <TextField label="Cargo" placeholder="Digite seu cargo" />
+                    <TextField label="Imagem" placeholder="Digite o endereço da imagem" />
+                </form>
+            </section>
+        );
+    }
 };
 
-export default form;
+export default Form;
