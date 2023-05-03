fe8983e5afe93d76f9642cc8a819438961a58d6d
Ramon Victor Cardoso <ramonvc19@gmail.com>
2023-05-02 23:06:47 -0300

    Dropdown added on Form

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,20 +1,34 @@
 import React from "react";
 import "./Form.css";
 import TextField from "../TextField";
+import Dropdown from "../Dropdown";
 
-class Form extends React.Component  {
-    render() {
-        return (
-            <section className="form">
-                <form>
-                    <h2>Preenche os dados para criar o card do jogador</h2>
-                    <TextField label="Nome" placeholder="Digite seu nome" />
-                    <TextField label="Cargo" placeholder="Digite seu cargo" />
-                    <TextField label="Imagem" placeholder="Digite o endereço da imagem" />
-                </form>
-            </section>
-        );
-    }
-};
+const ranks = [
+	"Ferro",
+	"Bronze",
+	"Prata",
+	"Ouro",
+	"Platina",
+	"Diamante",
+	"Mestre",
+	"Grão-mestre",
+	"Desafiante",
+];
+
+class Form extends React.Component {
+	render() {
+		return (
+			<section className="form">
+				<form>
+					<h2>Preenche os dados para criar o card do jogador</h2>
+					<TextField label="Nome" placeholder="Digite seu nome" />
+					<TextField label="Cargo" placeholder="Digite seu cargo" />
+					<TextField label="Imagem" placeholder="Digite o endereço da imagem" />
+					<Dropdown label="Rank" items={ranks} />
+				</form>
+			</section>
+		);
+	}
+}
 
 export default Form;
