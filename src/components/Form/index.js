6ed2f365cd68ba0834694e94666a7df8be13404e
Ramon Victor Cardoso <ramonvc19@gmail.com>
2023-05-04 01:13:34 -0300

    Added props to form components

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,40 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Form.css";
 import TextField from "../TextField";
 import Dropdown from "../Dropdown";
 import Button from "../Button";
 
-const ranks = [
-	"Ferro",
-	"Bronze",
-	"Prata",
-	"Ouro",
-	"Platina",
-	"Diamante",
-	"Mestre",
-	"Grão-mestre",
-	"Desafiante",
-];
+const Form = () => {
+	const [name, setName] = useState("");
+	const [position, setPosition] = useState("");
+	const [image, setImage] = useState("");
+	const [rank, setRank] = useState("");
 
-const onSave = (event) => {
-	event.preventDefault();
+	const ranks = [
+		"Ferro",
+		"Bronze",
+		"Prata",
+		"Ouro",
+		"Platina",
+		"Diamante",
+		"Mestre",
+		"Grão-mestre",
+		"Desafiante",
+	];
+
+	const onSave = (event) => {
+		event.preventDefault();
+	};
+
+	return (
+		<section className="form">
+			<form onSubmit={onSave}>
+				<h2>Preenche os dados para criar o card do jogador</h2>
+				<TextField
+					label="Nome"
+					placeholder="Digite seu nome"
+					required={true}
+					value={name}
+					onInput={(value) => setName(value)}
+				/>
+				<TextField
+					label="Cargo"
+					placeholder="Digite seu cargo"
+					required={true}
+					value={position}
+					onInput={(value) => setPosition(value)}
+				/>
+				<TextField
+					label="Imagem"
+					placeholder="Digite o endereço da imagem"
+					value={image}
+					onInput={(value) => setImage(value)}
+				/>
+				<Dropdown 
+					label="Rank" 
+					items={ranks} 
+					value={rank} 
+					onChanged={(value) => setRank(value)} 
+				/>
+				<Button>Criar Card</Button>
+			</form>
+		</section>
+	);
 };
-
-class Form extends React.Component {
-	render() {
-		return (
-			<section className="form">
-				<form onSubmit={onSave}>
-					<h2>Preenche os dados para criar o card do jogador</h2>
-					<TextField label="Nome" placeholder="Digite seu nome" required={true} />
-					<TextField label="Cargo" placeholder="Digite seu cargo" required={true} />
-					<TextField label="Imagem" placeholder="Digite o endereço da imagem" />
-					<Dropdown label="Rank" items={ranks} />
-					<Button>Criar Card</Button>
-				</form>
-			</section>
-		);
-	}
-}
 
 export default Form;
