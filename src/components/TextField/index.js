41b45ec55b49cafdbea424716cf1f0cb556e1b21
Ramon Victor Cardoso <ramonvc19@gmail.com>
2023-05-04 01:03:18 -0300

    Added value and onInput props to TextField

--- src/components/TextField/index.js
+++ src/components/TextField/index.js
@@ -1,10 +1,19 @@
 import "./TextField.css";
 
 const TextField = (props) => {
+	const onInput = (event) => {
+		props.onChanged(event.target.value);
+	};
+
 	return (
 		<div className="text-field">
 			<label>{props.label}</label>
-			<input placeholder={props.placeholder} required={props.required} />
+			<input
+				onChange={onInput}
+				value={props.value}
+				placeholder={props.placeholder}
+				required={props.required}
+			/>
 		</div>
 	);
 };
