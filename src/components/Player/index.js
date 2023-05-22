954487d1ab48c741baa50ab29746d6c464a35ae2
Ramon Victor Cardoso <ramonvc19@gmail.com>
2023-05-22 00:02:18 -0300

    Added default icons to replace images with error or empty url

--- src/components/Player/index.js
+++ src/components/Player/index.js
@@ -1,17 +1,31 @@
 import "./Player.css";
+import React, { useState } from 'react';  
+  
+const Player = ({ name, position, image, primaryColor }) => {  
+	const [imageUrl, setImageUrl] = useState(image);  
+	
+	// Generate a random number between 1 and 10  
+	const randomIconNumber = Math.floor(Math.random() * 10) + 1;  
+  
+	// Build the default icon path using the random number  
+	const defaultImage = `${process.env.PUBLIC_URL}/icons/icon-${randomIconNumber}.webp`; 
+  
+	const handleImageError = () => {  
+		setImageUrl(defaultImage);  
+	};  
+  
+	return (  
+		<div className="player">  
+			<div className="header" style={{ backgroundColor: primaryColor }}>  
+				<img src={imageUrl} alt={name} onError={handleImageError} />  
+			</div>  
+			<div className="footer">  
+				<h4 className="name" style={{ color: primaryColor }}>{name}</h4>  
+				<h5 className="position">{position}</h5>  
+			</div>  
+		</div>  
+	);  
+};  
+  
+export default Player;  
 
-const Player = ({ name, position, image, primaryColor }) => {
-	return (
-		<div className="player">
-			<div className="header" style={{ backgroundColor: primaryColor }}>
-				<img src={image} alt={name} />
-			</div>
-			<div className="footer">
-				<h4 className="name" style={{ color: primaryColor }}>{name}</h4>
-				<h5 className="position">{position}</h5>
-			</div>
-		</div>
-	);
-};
-
-export default Player;
