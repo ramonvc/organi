85f32dd73aa8635fff56d2f23de906b60fa416a1
Ramon Victor Cardoso <ramonvc19@gmail.com>
2023-05-21 23:27:05 -0300

    Changed the style player

--- src/components/Player/index.js
+++ src/components/Player/index.js
@@ -1,14 +1,14 @@
 import "./Player.css";
 
-const Player = ({ name, position, image, backgroundColor }) => {
+const Player = ({ name, position, image, primaryColor }) => {
 	return (
 		<div className="player">
-			<div className="header" style={{ backgroundColor: backgroundColor }}>
+			<div className="header" style={{ backgroundColor: primaryColor }}>
 				<img src={image} alt={name} />
 			</div>
 			<div className="footer">
-				<h4>{name}</h4>
-				<h5>{position}</h5>
+				<h4 className="name" style={{ color: primaryColor }}>{name}</h4>
+				<h5 className="position">{position}</h5>
 			</div>
 		</div>
 	);
