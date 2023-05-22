3bbacd75dcdad1579767140aa2010a8dc6a90ee6
Ramon Victor Cardoso <ramonvc19@gmail.com>
2023-05-22 20:13:05 -0300

    Added border effect in players cards

--- src/components/Player/index.js
+++ src/components/Player/index.js
@@ -1,31 +1,36 @@
 import "./Player.css";
-import React, { useState } from 'react';  
-  
-const Player = ({ name, position, image, primaryColor }) => {  
-	const [imageUrl, setImageUrl] = useState(image);  
-	
-	// Generate a random number between 1 and 10  
-	const randomIconNumber = Math.floor(Math.random() * 10) + 1;  
-  
-	// Build the default icon path using the random number  
-	const defaultImage = `${process.env.PUBLIC_URL}/icons/icon-${randomIconNumber}.webp`; 
-  
-	const handleImageError = () => {  
-		setImageUrl(defaultImage);  
-	};  
-  
-	return (  
-		<div className="player">  
-			<div className="header" style={{ backgroundColor: primaryColor }}>  
-				<img src={imageUrl} alt={name} onError={handleImageError} />  
-			</div>  
-			<div className="footer">  
-				<h4 className="name" style={{ color: primaryColor }}>{name}</h4>  
-				<h5 className="position">{position}</h5>  
-			</div>  
-		</div>  
-	);  
-};  
-  
-export default Player;  
+import React, { useState } from "react";
 
+const Player = ({ name, position, image, primaryColor }) => {
+	const [imageUrl, setImageUrl] = useState(image);
+
+	// Generate a random number between 1 and 10
+	const randomIconNumber = Math.floor(Math.random() * 10) + 1;
+
+	// Build the default icon path using the random number
+	const defaultImage = `${process.env.PUBLIC_URL}/icons/icon-${randomIconNumber}.webp`;
+
+	const handleImageError = () => {
+		setImageUrl(defaultImage);
+	};
+
+	return (
+		<div
+			className="player border-effect"
+			style={{
+				"--borderColor": primaryColor,
+			}}>
+			<div className="header" style={{ backgroundColor: primaryColor }}>
+				<img src={imageUrl} alt={name} onError={handleImageError} />
+			</div>
+			<div className="footer">
+				<h4 className="name" style={{ color: primaryColor }}>
+					{name}
+				</h4>
+				<h5 className="position">{position}</h5>
+			</div>
+		</div>
+	);
+};
+
+export default Player;
