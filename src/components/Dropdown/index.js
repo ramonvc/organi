7c23ff8e3be52e9ffe76978aead8ab898cf519b2
Ramon Victor Cardoso <ramonvc19@gmail.com>
2023-05-22 19:34:27 -0300

    Fix: Dropdown submitting empty value

--- src/components/Dropdown/index.js
+++ src/components/Dropdown/index.js
@@ -3,13 +3,16 @@ import "./Dropdown.css";
 
 class Dropdown extends React.Component {
 	render() {
-		const { label, onChanged, value, items} = this.props;
+		const { label, onChanged, value, items } = this.props;
 		return (
 			<div className="dropdown">
 				<label>{label}</label>
-				<select onChange={(event) => onChanged(event.target.value)} value={value}>
+				<select onChange={(event) => onChanged(event.target.value)} value={value} required>
+					<option value="" disabled hidden>
+						Selecione
+					</option>
 					{items.map((item) => (
-						<option key={item.name} style={{ color: item.primaryColor }}>
+						<option key={item.name} value={item.name} style={{ color: item.primaryColor }}>
 							{item.name}
 						</option>
 					))}
